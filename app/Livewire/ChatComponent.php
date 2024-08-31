decf504fe46f51321c22abf8cef2cbc27f836412
Masud Rana Tapu <masudranatapu@gmail.com>
2024-08-31 23:31:57 +0600

    doen

---
+++ app/Livewire/ChatComponent.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\User;
+use Livewire\Component;
+
+class ChatComponent extends Component
+{
+    public $user;
+    public function render()
+    {
+        return view('livewire.chat-component');
+    }
+
+    public function mount($user_id)
+    {
+        $this->user = User::findOrFail($user_id);
+    }
+}
